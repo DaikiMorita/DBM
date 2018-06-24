b089776b043a074b83a067ff4aca6e66374c9fae
DaikiMorita <cube-0825@hotmail.co.jp>
2018-06-25 00:37:47 +0900

    create package

---
+++ python/src/Application.py
@@ -0,0 +1,14 @@
+from python.src.Controller import ParamSetController
+
+
+class Application(object):
+    """
+    Deep Boltzmann Machine Project
+    """
+
+    def __init__(self):
+        ParamSetController.ParamSetController()
+
+
+if __name__ == '__main__':
+    Application = Application()
---
+++ python/src/Controller/ParamSetController.py
@@ -0,0 +1,4 @@
+class ParamSetController(object):
+
+    def __init__(self):
+        pass
