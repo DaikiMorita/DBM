5b2134729477954dde10b0309a1caed7cba332c0
DaikiMorita <cube-0825@hotmail.co.jp>
2018-06-30 01:22:14 +0900

    modified setup.py

--- DBM/test/ManagerTest/PreProcessManagerTest.py
+++ DBM/test/ManagerTest/PreProcessManagerTest.py
@@ -9,19 +9,25 @@ class PreProcessManagerTest(unittest.TestCase):
     unit test of features in PreProcessManager
     """
 
+    def setUp(self):
+        """
+        procedures before every tests are started. This code block is executed only once.
+        :return:
+        """
+        self.pre_process_manager = PreProcessManager.PreProcessManager()
+
     def test_z_score_normalization(self):
         """
         test of z-score normalization to 2-d numpy array
         :return: None
         """
-        pre_process_manager = PreProcessManager.PreProcessManager()
 
         # array normalized into z-score
         arr = np.array([[0, 2, 2, 4], [0, 2, 2, 4]])
 
         expected = np.array([[-np.sqrt(2), 0, 0, np.sqrt(2)], [-np.sqrt(2), 0, 0, np.sqrt(2)], ])
 
-        actual = pre_process_manager.z_score_normalization(arr)
+        actual = self.pre_process_manager.z_score_normalization(arr)
 
         npt.assert_array_almost_equal(expected, actual)
 
@@ -30,8 +36,6 @@ class PreProcessManagerTest(unittest.TestCase):
         test of making mini batches from 2-d list
         :return: None
         """
-
-        pre_process_manager = PreProcessManager.PreProcessManager()
 
         # 2-d list
         data_list = [[1, 1, 1],
@@ -49,6 +53,6 @@ class PreProcessManagerTest(unittest.TestCase):
         expected = [[[1, 1, 1], [2, 2, 2], [3, 3, 3]], [[4, 4, 4], [5, 5, 5], [6, 6, 6]],
                     [[7, 7, 7], [8, 8, 8], [9, 9, 9]], [[10, 10, 10], [1, 1, 1], [2, 2, 2]]]
 
-        actual = pre_process_manager.make_mini_batch(data_list, 3)
+        actual = self.pre_process_manager.make_mini_batch(data_list, 3)
 
         self.assertEqual(expected, actual)
--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ sys.path.append('./DBM/test')
 setup(
     name="Deep Boltzmann Machine",
     version="0.1",
+    description="Deep Boltzmann Machine, referring a paper called"
+                "'Efficient Learning of Deep Boltzmann Machines' by Salakhutdinov and Larochelle in 2010",
+    author="Daiki Morita",
+    url="https://github.com/DaikiMorita/RBM",
     packages=find_packages(),
+    install_requires=['numpy', 'tqdm', 'requests'],
+    dependency_links="",
     test_suite='DBM.test.suite.suite'
 )
