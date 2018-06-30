38e324689d65632a2de2e2ff4b97c6f3669e36b6
DaikiMorita <cube-0825@hotmail.co.jp>
2018-06-30 22:38:59 +0900

    configparser

--- DBM/src/Controller/MainController.py
+++ DBM/src/Controller/MainController.py
@@ -1,18 +1,17 @@
+import configparser
+
+
 class MainController(object):
     """
     Controls main process
     """
 
     def __init__(self):
-        pass
+        app_config = configparser.ConfigParser()
+        learning_config = configparser.ConfigParser()
 
     def start_main_pricess(self):
         """
         Main Process:
         :return:
         """
-
-
-
-
-
