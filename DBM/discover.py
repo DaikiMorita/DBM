0e0548bcd9d237bc8b9ee84214ff970158df643e
DaikiMorita <cube-0825@hotmail.co.jp>
2018-06-30 19:49:30 +0900

    modified search root of test suite discover

--- DBM/discover.py
+++ DBM/discover.py
@@ -6,9 +6,8 @@ def suite():
     # 前述と同じく、TestSuiteから空っぽのテストスイートを作成します
     test_suite = unittest.TestSuite()
     # discoverメソッドを用いて、testディレクトリ以下からテストクラスを見つけます
-    all_test_suite = unittest.defaultTestLoader.discover("test", pattern="test_*.py")
+    all_test_suite = unittest.defaultTestLoader.discover("DBM.test", pattern="test_*.py")
     # 見つけたテストクラスをテストスイートに追加します
     for ts in all_test_suite:
         test_suite.addTest(ts)
     return test_suite
-
