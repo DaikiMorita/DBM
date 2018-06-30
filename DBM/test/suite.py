b6454c887f53b9189e58881f827b4ff85266db3f
DaikiMorita <cube-0825@hotmail.co.jp>
2018-06-30 19:26:21 +0900

    add PostProcessTest into test suite

--- DBM/test/suite.py
+++ DBM/test/suite.py
@@ -1,10 +1,12 @@
 import unittest
 from DBM.test.ManagerTest import PreProcessManagerTest
+from DBM.test.ManagerTest import PostProcessManagerTest
 
 
 def suite():
     # テストスイートを定義します
     test_suite = unittest.TestSuite()
     # addTestを用いてテストスイートに追加していきます
-    test_suite.addTest(unittest.makeSuite(PreProcessManagerTest.PreProcessManagerTest))
+    test_suite.addTest(
+        unittest.makeSuite(PreProcessManagerTest.PreProcessManagerTest, PostProcessManagerTest.PostProcessManagerTest))
     return test_suite
