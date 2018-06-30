198d5b6948d431a393809db163ae8eb20bc6d132
DaikiMorita <cube-0825@hotmail.co.jp>
2018-06-30 20:02:53 +0900

    modified comments on discovery.py

--- DBM/discover.py
+++ DBM/discover.py
@@ -1,13 +1,14 @@
 import unittest
 
 
-# テストスイートを作成します
 def suite():
-    # 前述と同じく、TestSuiteから空っぽのテストスイートを作成します
+    """
+    creates test suite
+    :return:
+    """
     test_suite = unittest.TestSuite()
-    # discoverメソッドを用いて、testディレクトリ以下からテストクラスを見つけます
+    # tries all test python files which match the specified pattern as a parameter
     all_test_suite = unittest.defaultTestLoader.discover("DBM.test", pattern="test_*.py")
-    # 見つけたテストクラスをテストスイートに追加します
     for ts in all_test_suite:
         test_suite.addTest(ts)
     return test_suite
