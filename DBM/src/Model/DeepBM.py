4d8b5a77d2569fd8c45efe8f0be486ed44096fe1
DaikiMorita <cube-0825@hotmail.co.jp>
2018-07-01 17:14:14 +0900

    add features in DeepBM

--- DBM/src/Model/DeepBM.py
+++ DBM/src/Model/DeepBM.py
@@ -7,28 +7,61 @@ class DeepBM(object):
     # Notice #
     ##########
 
-    # you can (or should) divide the methods below or add some additional methods
-    # ,aiming at more simple and readable codes.
+    # you can (or should) divide the methods below or add some additional methods,
+    # aiming at more simple and readable codes and easier test codes.
 
-    # somewhere with less matrix calculation should be done by C/C++.
+    # somewhere with less matrix calculation or with too many "for" loops should be done by C/C++.
+    # numpy is good at matrix calculation but runs too slowly inside "for" loop.
 
     def __init__(self, num_hidden_layer):
         self.num_hidden_layer = num_hidden_layer
 
     # To Do: this code block below will control the whole learning process described in the article.
     def learning(self):
+        # call pre_training
+
+        # iterate T times below
+        # variational_inference
+        # stochastic_approximation
+        # parameter_update
+        # decrease_alpha
+
         pass
 
-    def pre_training(self):
+    def greedy_pre_training(self):
         pass
 
-    # To Do: must be better to add a method in charge of a calculation of KL divergence
     def variational_inference(self):
+        pass
+
+    # To Do: eq. (8)~(10)
+    def sigmoid_activation(self):
+        pass
+
+    # To Do: eq. (8)~(10)
+    def calc_bottom_up_pass(self):
+        pass
+
+    # To Do: eq. (4)~(6)
+    def solve_mean_field_fixed_point_equation(self):
+        pass
+
+    # To Do: eq. (11)
+    def calc_KL_divergence(self):
         pass
 
     def stochastic_approximation(self):
         pass
 
+    def gibbs_sampler(self):
+        pass
+
     def parameter_update(self):
         pass
 
+    # To Do: inside () at parameter_update
+    def calc_gradient(self):
+        pass
+
+    def decrease_alpha(self):
+        pass
